8b71d1cf22310707e02374cfc6b7b03b8e434b4d
Durgeshgour123 <145599156+Durgeshgour123@users.noreply.github.com>
2023-09-20 23:05:01 +0530

    Add files via upload

---
+++ string1.java
@@ -0,0 +1,12 @@
+class sample{
+          public static void main(String[] args){
+              
+              char[]c={'a','b','c'};
+              String s2=new String(c);
+          
+             
+            
+              System.out.println(s2);
+          
+    }
+}
