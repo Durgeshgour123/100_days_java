b1bb688648e37b92a7ad5335a8365597487b8d98
Durgeshgour123 <145599156+Durgeshgour123@users.noreply.github.com>
2023-09-21 17:49:04 +0530

    Add files via upload

---
+++ prog5.java
@@ -0,0 +1,8 @@
+ class sample {
+    public static void main(String[] args) {
+        for (char c = 'A'; c <= 'Z'; c++) {
+            int asciiValue = (int) c;
+            System.out.println(c + "-" + asciiValue);
+        }
+    }
+}
