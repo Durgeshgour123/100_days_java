8a444f4899ebd482a16bcede7c24e1be298c12e4
Durgeshgour123 <145599156+Durgeshgour123@users.noreply.github.com>
2023-09-23 14:42:05 +0530

    Add files via upload

---
+++ positiveneg.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class  solution{
+            public static void main (String... args){
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter Number:");
+            int number=sc.nextInt();
+   
+            if(number>0){
+            System.out.println("Positive");
+            }
+            else{
+                   System.out.println("negitive");
+              }
+         } 
+    }
+             
+               
