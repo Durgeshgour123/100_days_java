91c321aa7b1244d55705149f4c142ed58587f1cc
Durgeshgour123 <145599156+Durgeshgour123@users.noreply.github.com>
2023-09-23 14:49:25 +0530

    Add files via upload

---
+++ multiplytwonum.java
@@ -0,0 +1,7 @@
+class Solution{
+             public static void main(String args[]){
+                  int num1=10;
+                   int num2=50;
+                 System.out.println(num1*num2);
+         }
+}
