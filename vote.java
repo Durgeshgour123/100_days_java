b357569df16de09144a6a41c20646d81659dc450
Durgeshgour123 <145599156+Durgeshgour123@users.noreply.github.com>
2023-09-24 17:15:38 +0530

    Add files via upload

---
+++ vote.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Solution{
+        public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter age");
+        int age=sc.nextInt();
+         if(age>=18)
+           {
+             System.out.println("yes");
+           }
+            else
+             {
+                 System.out.println("no");
+             }
+    
+   }
+ }
